494ee2c74105fe92aaa2e1763ee8d7bed94c3c2b
07734willy <piortau@gmail.com>
2019-04-29 22:02:09 -0500

    fixed imports in report_var_table

--- ActModel_0.0.1/report_var_table.py
+++ ActModel_0.0.1/report_var_table.py
@@ -4,18 +4,9 @@ Created on Sat Mar 16 15:33:14 2019
 
 @author: User
 """
-#import xml.etree.ElementTree as xmlET
-import sys,os
-from mTable import Table
-import pandas as pandas
-from io import StringIO
-import collections
-#import pandas
-'''
-        from pathlib import Path
-        import xml.etree.ElementTree as xmlET
-      import re 
-      '''
+from table import Table
+from collections import OrderedDict
+
 class ReportVarTable(Table):
     """
     This class reads data from XML files. It contains several groups of variables. 
@@ -23,7 +14,7 @@ class ReportVarTable(Table):
 	
     def __init__(self,*args,**kwargs): #sTableDir=".",        
         #super().__init__(self,sTableName=sTableName)   
-        self.odReportVars=collections.OrderedDict()#ALIAS FOR SELF.BODY
+        self.odReportVars = OrderedDict()#ALIAS FOR SELF.BODY
         super().__init__(*args,**kwargs)
         
         
@@ -31,7 +22,7 @@ class ReportVarTable(Table):
         return super()._fXMLFindChildOutputList(*args,**kwargs)
     
     def _fXMLReadBody(self):
-        odTemp=collections.OrderedDict()
+        odTemp = OrderedDict()
         for xmlVar in self._xmlRoot.find("BODY").findall("REPORT_VARS"): 
             lAccumVal=xmlVar.text.strip().split(",")
             odTemp[xmlVar.attrib["name"]]=lAccumVal        
